021aeee586c2e223555964551928763fcee6f08d
Diego Fonseca <diegosantosfonseca@hotmail.com>
2018-11-06 12:31:44 -0200

    Projeto criado

---
+++ CURSO_XADREZ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURSO_XADREZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
