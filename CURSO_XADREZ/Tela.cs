77ffc97f93e9a26635f178c072e08e589a23c215
Diego Fonseca <diegosantosfonseca@hotmail.com>
2018-11-12 14:27:51 -0200

    Acrescentando as demais pecas

--- CURSO_XADREZ/Tela.cs
+++ CURSO_XADREZ/Tela.cs
@@ -21,12 +21,12 @@ namespace CURSO_XADREZ
                 {
                     Console.WriteLine("XEQUE!");
                 }
-                else
-                {
-                    Console.WriteLine("XEQUEMATE!");
-                    Console.WriteLine("Vencedor: " + partida.jogadorAtual);
-                }
             }
+            else
+            {
+                Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Vencedor: " + partida.jogadorAtual);
+            }            
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
