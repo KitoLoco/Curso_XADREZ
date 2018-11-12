2ac00fa215f74e597b6f22681162ce27941b42b8
Diego Fonseca <diegosantosfonseca@hotmail.com>
2018-11-12 15:56:46 -0200

    Jogada especial roque pequeno e roque grande

--- CURSO_XADREZ/xadrez/Rei.cs
+++ CURSO_XADREZ/xadrez/Rei.cs
@@ -4,8 +4,11 @@ namespace xadrez
 {
     class Rei : Peca // classe Rei.cs é uma SubClasse da classe Peca (Herança)
     {
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor) // : base repassa para construtor da classe PAI (Peca)
+        private PartidaDeXadrez partida;
+
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor) // : base repassa para construtor da classe PAI (Peca)
         {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -19,6 +22,11 @@ namespace xadrez
             return p == null || p.cor != this.cor;
         }
 
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
+        }
 
         public override bool[,] movimentosPossiveis() // aqui q enviamos para a classe PAI Peca.cs o que o metodo abstract deve fazer
         {
@@ -74,11 +82,37 @@ namespace xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
             }
+
+            //#jogadaespecial Roque
+            if (qteMovimentos==0 && !partida.xeque)
+            {
+                // #jogadaespecial Roque Pequeno
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                if (testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if (tab.peca(p1)==null && tab.peca(p2)==null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+
+                // #jogadaespecial Roque Grande
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if (testeTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
+            }
+
             return mat;
-
         }
-
-
-
     }
 }
